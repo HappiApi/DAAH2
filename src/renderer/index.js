c298bdd36d1c3f952885bccf1541b878808a2f54
Axel Goetz <axel.goetz@hotmail.com>
2016-01-12 16:37:04 +0000

    Dragging and dropping

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,1 +1,42 @@
-console.log("Hello world!");
+const $ = require('jquery');
+
+var currentDragElement;
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    currentDragElement = ev.target;
+}
+
+function drop(ev) {
+    ev.preventDefault();
+
+    var element = currentDragElement;
+
+    if(element != null) {
+      if($(element).hasClass("drag-element")) {
+        $(ev.target).append($('<img src="' + element.src + '" alt="' + element.alt + '">').on("dragstart", drag));
+      }
+      else {
+        $(element).appendTo($(ev.target));
+      }
+    }
+
+    currentDragElement = null;
+}
+
+var grid = $('#grid');
+for(var i = 0; i < 9; i++) {
+    var row = $('<div class="row">');
+    grid.append(row);
+    for(var j = 0; j < 9; j++) {
+      row.append('<div class="column"></div>')
+        .on("dragover", allowDrop)
+        .on("drop", drop)
+    }
+    grid.append('</div>');
+}
+
+$('.drag-element').on("dragstart", drag);
