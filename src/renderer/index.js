652d99ea9fc1678302e0bc597f21be9f9722aec6
Axel Goetz <axel.goetz@hotmail.com>
2016-01-13 16:59:09 +0000

    Added rotating

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,106 +1,41 @@
 const $ = require('jquery');
 const d3 = require('d3');
 
-var currentDragElement = null;
 var selectedElement = null;
-
-// Dropping and Dragging Code
-//-----------------------------------------------
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drag(ev) {
-  currentDragElement = ev.target;
-}
-
-function checkTarget(ev) {
-  while(!$(ev.target).hasClass("column")) {
-    console.log(ev.target);
-    ev.target = ev.target.parentElement;
-  }
-  $(ev.target).empty();
-}
-
-function drop(ev) {
-  ev.preventDefault();
-
-  var element = currentDragElement;
-  checkTarget(ev);
-
-  if(element != null) {
-    if($(element).hasClass("drag-element")) {
-      var img = $('<img src="' + element.src + '" alt="' + element.alt + '">');
-      img.on("dragstart", drag);
-      selectedElement = img;
-      $(ev.target).append(img);
-    }
-    else {
-      $(element).appendTo($(ev.target));
-      selectedElement = element;
-    }
-    generateRightBar();
-  }
-
-  currentDragElement = null;
-}
-
-//Setup
-//------------------
-
-var grid = $('#grid');
-// for(var i = 0; i < 9; i++) {
-//   var row = $('<div class="row">');
-//   grid.append(row);
-
-//   for(var j = 0; j < 9; j++) {
-//     var column = $('<div class="column"></div>');
-//     column
-//       .on("dragover", allowDrop)
-//       .on("drop", drop)
-//       .on("click", selectElement);
-//     row.append(column);
-//   }
-
-//   grid.append('</div>');
-// }
-
-$('.drag-element').on("dragstart", drag);
-
+var components = [];
 
 //Right bar Code
 //-----------------------------------------------
 
-
 function getRotationDegrees(obj) {
-  var matrix = obj.css("transform");
-  var values = matrix.split('(')[1].split(')')[0].split(',');
-  var a = values[0];
-  var b = values[1];
-  var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-  return (angle < 0) ? angle +=360 : angle;
+  var translate = d3.transform(obj.attr("transform"));
+  var angle = translate['rotate'];
+  return angle;
 }
 
 function rotating(ev) {
   if($(ev.target).attr('id') == 'rotate-cw') {
-    var rotation = getRotationDegrees($(selectedElement)) + 90;
+    var rotation = ((getRotationDegrees(draggedElement) + 90) % 360);
   }
   else {
-    var rotation = getRotationDegrees($(selectedElement)) - 90;
+    var rotation = ((getRotationDegrees(draggedElement) - 90) % 360);
   }
-  $(selectedElement).css("transform", "translate(-50%, -50%) rotate(" + rotation + "deg)");
+  var translate = d3.transform(draggedElement.attr("transform"))['translate'];
+
+  draggedElement.attr("transform", "translate(" + translate[0] + "," + translate[1] + ") rotate("
+   + rotation + ", " + 0 + "," + 0 +")");
 }
 
 function deleteElement(ev) {
-  $(selectedElement).remove();
-  selectedElement = null;
+  $(draggedElement.node()).remove();
+  draggedElement = null;
   generateRightBar();
 }
 
 function generateRightBar() {
   var container = $('#component-specific');
   container.empty();
-  if(selectedElement != null) {
+  if(draggedElement != null) {
     container.append('<h1>' + 'Name' + '</h1>');
 
     //if(component.name == 'resistor') {
@@ -127,15 +62,6 @@ function generateRightBar() {
     //}
   }
 }
-
-
-function selectElement(ev) {
-  selectedElement = ev.target;
-  generateRightBar();
-}
-
-//Setup
-//------------------
 
 $('#rotate-acw').on('click', rotating);
 $('#rotate-cw').on('click', rotating);
@@ -169,18 +95,20 @@ function dragstart() {
       .attr("height", 40)
       .attr("xlink:href", current.node().src)
   }
+  generateRightBar();
 }
 
 function dragmove() {
-  console.log("dragmove", d3.event);
+  //console.log("dragmove", d3.event);
   var pos = d3.mouse(svg.node());
   draggedElement
-    .attr("transform", "translate(" + (pos[0] - 20) + "," + (pos[1] - 20) + ")")
+    .attr("transform", "translate(" + (pos[0] - 20) + "," + (pos[1] - 20) + ")" + "rotate("
+     + getRotationDegrees(draggedElement) + ", " + 20 + "," + 20 +")")
 }
 
 function dragend() {
-  console.log("dragend", d3.event);
-  draggedElement = null;
+  //console.log("dragend", d3.event);
+  //draggedElement = null;
 }
 
 
@@ -193,6 +121,7 @@ function resize() {
 
 resize();
 
+$(window).resize(resize);
 drag.on("dragstart", dragstart);
 drag.on("drag", dragmove);
 drag.on("dragend", dragend);
